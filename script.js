b3d91da70b916c1e4f47894b44c6b37970162e0f
ssanic <73366301+ssanic@users.noreply.github.com>
2021-12-08 23:13:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
+
+
+const ratingItemsList = document.querySelectorAll('.rating__item');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach(item =>
+    item.addEventListener('click', () =>{
+        const { itemValue } = item.dataset;
+        item.parentNode.dataset.totalValue = itemValue;
+    })
+    );
+
+   new Swiper('.image-slider');
